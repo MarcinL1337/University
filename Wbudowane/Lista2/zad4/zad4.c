6441e6281fe749e6a9ac1e420626cd7878e2eaeb
MarcinL1337 <marcin.linkiewicz1337@gmail.com>
2023-10-18 18:42:11 +0200

    updates

--- Wbudowane/Lista2/zad4/zad4.c
+++ Wbudowane/Lista2/zad4/zad4.c
@@ -5,26 +5,17 @@
 #define BAUD 9600                            // baudrate
 #define UBRR_VALUE ((F_CPU) / 16 / (BAUD)-1) // zgodnie ze wzorem
 
-#define A 0b11111110
-#define B 0b11111101
-#define C 0b11111011
-#define D 0b11110111
-#define E 0b11101111
-#define F 0b11011111
-#define G 0b10111111
-#define O 0b01111111
-
 const uint8_t digits[10] = {
-    A & B & C & D & E & F,
-    B & C,
-    A & B & G & E & D,
-    A & B & G & C & D,
-    F & G & B & C,
-    A & F & G & C & D,
-    A & F & G & C & D & E,
-    A & B & C,
-    A & B & C & D & E & F & G,
-    A & B & G & F & C & D
+    0b00111111,
+    0b00000110,
+    0b01011011,
+    0b01001111,
+    0b01100110,
+    0b01101101,
+    0b01111101,
+    0b00000111,
+    0b01111111,
+    0b01101111
 };
 
 int main(){
@@ -35,10 +26,10 @@ int main(){
         for(int i = 0; i < 60; i++){
             for(int j = 0; j < 50; j++){
                 PORTC = 0b11111101;
-                PORTD = digits[i%10];
+                PORTD = ~digits[i%10];
                 _delay_ms(10);
                 PORTC = 0b11111110;
-                PORTD = digits[i/10];
+                PORTD = ~digits[i/10];
                 _delay_ms(10);
             }
         }
