f8193b2bdf0f8c9f50f0e9214dbbd3159f666e23
Marcin <marcin.linkiewicz1337@gmail.com>
2023-11-08 00:29:33 +0100

    Budowa Lista 4 all in es?

--- Wbudowane/Lista4/zad3/script.py
+++ Wbudowane/Lista4/zad3/script.py
@@ -6,7 +6,7 @@ a = np.arange(0, 6.283, 0.01)
 
 
 def h(x):
-    if x < 50 * math.pi:
+    if x < 25 * math.pi:
         return 50 * math.sin(x / 50 + math.pi/2) + 50
     elif x > 50 * math.pi:
         return 50 * math.sin(x / 50 - math.pi) + 50
@@ -16,8 +16,6 @@ def h(x):
 
 counter = 0
 
-for i in np.arange(0, 50 * 150 * math.pi, 10):
+for i in np.arange(0, 75 * math.pi, 0.5):
     print(math.floor(h(i)), end=", ")
     counter += 1
-
-# print(counter)
---
+++ Wbudowane/Lista4/zad4/zad4.c
@@ -0,0 +1,52 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LED PB5
+#define LED_DDR DDRB
+#define LED_PORT PORTB
+
+void timer1_init()
+{
+  // ustaw tryb licznika
+  // COM1A = 10   -- non-inverting mode
+  // WGM1  = 1110 -- fast PWM top=ICR1
+  // CS1   = 101  -- prescaler 1024
+  // ICR1  = 15624
+  // częstotliwość 16e6/(1024*(1+15624)) = 1 Hz
+  // wzór: datasheet 20.12.3 str. 164
+//   ICR1 = 15624;
+  OCR1A = 421; // 16000000 / 37900 = 1 + 421
+  TCCR1A = _BV(COM1B1) | _BV(WGM11) | _BV(WGM10);
+  TCCR1B = _BV(WGM12) | _BV(WGM13) | _BV(CS10) | _BV(ICES1);
+  // ^ Fast PWM Mode, top = OCR1A. ICES1 -> wartość licznika kopiowana do ICR1 przy wzroście sygnału
+  // ustaw pin OC1A (PB1) jako wyjście
+  DDRB |= _BV(PB2);
+}
+
+// ICP1 zczytuje wartość licznika jak przychodzi impuls z detektora i zapisuje do ICR1
+// Czujnik jak coś wykryje to puszcza sygnał do D8 (ICP1). ICP1 to port zegara i jak przychodzi sygnał, to
+// odpowiednio ustawiony (_BV(ICES1)), zapisuje swoją aktualną wartość do ICR1, więc jak ICR1 zmienia wartość, na inną
+// to znaczy, że coś wykrył.
+
+int main()
+{
+  // uruchom licznik
+  timer1_init();
+  uint16_t aux = ICR1;
+  OCR1B = 211;
+  LED_DDR = _BV(LED);
+  while(1){
+    for(uint16_t i = 0; i < 6; i++){
+      LED_PORT &= ~_BV(LED);
+      if(aux != ICR1){ // Detektor coś wykrył (zmieniła się wartość)
+        LED_PORT |= _BV(LED);
+        aux = ICR1;
+      }
+      LED_DDR |= _BV(PB2);
+      _delay_us(600);
+      LED_DDR &= ~_BV(PB2);
+      _delay_us(600);
+    }
+    _delay_ms(100);
+  }
+}
