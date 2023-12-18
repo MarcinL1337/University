64b2611c5c95d58cf8277d0405f2e5ffe7371f86
Marcin <marcin.linkiewicz1337@gmail.com>
2023-12-18 20:36:41 +0100

    Budowa L10 Z2

--- Wbudowane/Lista10/zad2/zad2.c
+++ Wbudowane/Lista10/zad2/zad2.c
@@ -6,8 +6,6 @@
 #include <avr/sleep.h>
 #include "hd44780.c"
 
-FILE uart_file;
-
 int hd44780_transmit(char data, FILE *stream)
 {
   LCD_WriteData(data);
@@ -16,8 +14,12 @@ int hd44780_transmit(char data, FILE *stream)
 
 void upload_symbols(){
   LCD_WriteCommand(HD44780_CGRAM_SET);
-  for(uint8_t i = 0; i < 6; i++)
-    LCD_WriteData(0x1F << i);
+  uint8_t aux = 0b00000;
+  for(uint8_t i = 0; i < 6; i++){
+    aux |= (1 << (5-i));
+    for(uint8_t j = 0; j < 8; j++)  
+      LCD_WriteData(aux);
+  }
 }
 
 void progress_bar(uint8_t percent){
@@ -25,15 +27,17 @@ void progress_bar(uint8_t percent){
   uint16_t cur_progress = (percent * 100) / 80;
   printf("%.2d", cur_progress);
   putchar(0b00100101);
+  // printf("%c", 0b00100101);
 
-  uint8_t aux = percent / 6;
+  uint8_t new_x = percent / 6;
   uint8_t aux2 = percent % 6;
   
-  LCD_GoTo(aux + 1, 0);
-  putchar(aux2);
-  
+  LCD_GoTo(new_x + 1, 0);
 
-  // putchar() tu skończyłem xd
+  if(new_x == 13) putchar(aux2 + 3);
+  else putchar(aux2);
+
+  // printf("%c", aux2);
 }
 
 FILE hd44780_file;
@@ -43,11 +47,11 @@ int main()
   // skonfiguruj wyświetlacz
   LCD_Initialize();
   LCD_Clear();
-  
   // skonfiguruj strumienie wyjściowe
   fdev_setup_stream(&hd44780_file, hd44780_transmit, NULL, _FDEV_SETUP_WRITE);
   stdout = stderr = &hd44780_file;
 
+  upload_symbols();
   while(1){
     for(uint8_t i = 0; i < 81; i++){
       progress_bar(i);
@@ -57,5 +61,3 @@ int main()
     LCD_Clear();
   }
 }
-
-
