a5bce642cfe2811faede8f7e322a4226e76a5423
MarcinL1337 <marcin.linkiewicz1337@gmail.com>
2024-01-10 19:34:02 +0100

    Budowa L11

--- Wbudowane/Lista11/zad2/zad2.c
+++ Wbudowane/Lista11/zad2/zad2.c
@@ -35,8 +35,8 @@ void adc_init(){
   ADMUX  |= _BV(REFS0); 
   // disable input buffers on pins A0 A1
   DIDR0   = _BV(ADC0D) | _BV(ADC1D); 
-  // prescaler 128
-  ADCSRA |= _BV(ADPS0) | _BV(ADPS1) | _BV(ADPS2); 
+  // prescaler
+  ADCSRA |= _BV(ADPS1); 
   // enable ADC
   ADCSRA |= _BV(ADEN); 
 }
@@ -46,15 +46,15 @@ void timer1_init(){
   TCCR1A |= _BV(COM1A1) | _BV(COM1A0);
   // PWM, Phase and Frequency Correct, ICR1 TOP | prescaler 8
   TCCR1B |= _BV(WGM13) | _BV(CS11);
-  ICR1 = 1024;
+  ICR1 = 2048;
   DDRB |= _BV(PB1);
 }
 
 ISR(TIMER1_CAPT_vect){
-  // disable Timer/Counter1 Input Capture interrupt  
-  TIMSK1 &= ~_BV(ICIE1);
   // start ADC conversion
   ADCSRA |= _BV(ADSC);
+  // disable Timer/Counter1 Input Capture interrupt  
+  TIMSK1 &= ~_BV(ICIE1);
 }
 
 ISR(TIMER1_OVF_vect){
@@ -77,8 +77,8 @@ int main()
   timer1_init();
   // przerwania
   sei();
-  uint8_t counter=0;
-  uint16_t ADC_lower,ADC_upper;
+  uint32_t counter=0;
+  uint32_t ADC_Bottom,ADC_Top;
   while(1) { 
     // set A0 as input
     ADMUX &= ~_BV(MUX0);  
@@ -86,9 +86,9 @@ int main()
     while (!(ADCSRA & _BV(ADIF))); // czekaj na wynik
     ADCSRA |= _BV(ADIF); // wyczyść bit ADIF (pisząc 1!)
     uint16_t v = ADC; // weź zmierzoną wartość (0..1023)
-    OCR1A = 2*ADC;
+    OCR1A = 2*v;
 
-    if(counter==100){
+    if(counter==10000){
       // set A1 as input
       ADMUX |= _BV(MUX0); 
       counter=0;
@@ -96,21 +96,18 @@ int main()
       TIMSK1 |= _BV(ICIE1);
       while (!(ADCSRA & _BV(ADIF))); 
       ADCSRA |= _BV(ADIF);
-      ADC_upper=ADC;
+      ADC_Bottom = ADC;
 
       // enable Timer/Counter1 Overflow interrupt
       TIMSK1 |= _BV(TOIE1);
       while (!(ADCSRA & _BV(ADIF))); 
       ADCSRA |= _BV(ADIF);
-      ADC_lower=ADC;
+      ADC_Top = ADC;
 
-      float f1=(float)ADC_upper*5.0 * 1000 / 1024.0,
-            f2=(float)ADC_lower*5.0 * 1000 / 1024.0;
-      printf("Napięcia H=%.4fmV,L=%.4fmV\r",f1,f2);
+      float top_voltage = (float)ADC_Top * 5.0 / 1024.0;
+      float bottom_voltage = 5 - ((float)ADC_Bottom * 5.0 / 1024.0);
+      printf("Engine voltage = %.4fV, Engine speed = %.4fV\r\n", top_voltage, bottom_voltage);
     }
-
     counter++;
-    _delay_ms(10);
   }
-
 }
