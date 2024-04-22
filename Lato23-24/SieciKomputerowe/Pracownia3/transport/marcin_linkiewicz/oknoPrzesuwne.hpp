46a99466ae6c88c3316b716cb6f41d252a4ccaa1
Marcin <marcin.linkiewicz1337@gmail.com>
2024-04-23 00:28:34 +0200

    Sieci transport guwno

---
+++ Lato23-24/SieciKomputerowe/Pracownia3/transport/marcin_linkiewicz/oknoPrzesuwne.hpp
@@ -0,0 +1,19 @@
+// Marcin Linkiewicz, 323853
+
+#include "datagram.hpp"
+
+static const int window_capacity = 600;
+
+class Okno{
+    private:
+        int size;
+
+    public:
+        std::list<Datagram> datagrams;
+        
+        Okno(int size);
+        void sendd(int sockfd, struct sockaddr_in *address);
+        void received(int start, int size, char *buffer);
+        void shift_window();
+
+};
